42287649ac8e4260d00fd820304b317dfbf96ab8
Daisy Koome <dskoome@gmail.com>
2021-11-10 04:27:36 +0300

    Added twitter credentials file which has the keys and tokens for validation

---
+++ twitter_credentials.py
@@ -0,0 +1,6 @@
+# Variables that contain user credentials for the Twitter API
+def credentials():
+    ACCESS_TOKEN="1031787320834486272-4rp0l8PIubzl3orECrFSXhH8UGwKvl"
+    ACCESS_TOKEN_SECRET="aecV9aw9mrQcZkVSs73gBCqPH7oH7JpWGHUTsPwtQyMa2"
+    CONSUMER_KEY="dH3QEI7Zrv5xgyM1JgNvPs7Ba"
+    CONSUMER_SECRET="oeN8HZJp3PIn3n0XZzMS7BePe4tI9FYz6ZBeIISAqV1YtpBMZI"
